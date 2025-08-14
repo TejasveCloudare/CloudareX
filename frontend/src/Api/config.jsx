8e5b9d3918c0bab696d62a8365bdaebbfc9eee1c
Tejasve <tejasve.g@cloudare.in>
2025-08-14 12:08:36 +0530

    config update

--- frontend/src/Api/config.jsx
+++ frontend/src/Api/config.jsx
@@ -1,5 +1,5 @@
-export const baseURL = "http://127.0.0.1:8000";
-// export const baseURL = "http://192.168.0.103:8000";
+// export const baseURL = "http://127.0.0.1:8000";
+export const baseURL = "https://cloudarex.onrender.com";
 
 const config = {
   baseURL: baseURL,
